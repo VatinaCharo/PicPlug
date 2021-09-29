01e6a4a01a80b6c68d25c17833a2f6aec6af3f8c
VatinaCharo <njuscimenphy@gmail.com>
2021-09-29 19:55:32 +0800

    push to v1.0.0 release

--- src/main/java/vcg/Utils/Resources.java
+++ src/main/java/vcg/Utils/Resources.java
@@ -5,7 +5,7 @@ public class Resources {
     public static final String NAME = "PicPlug";
     public static final String AUTHOR = "Vatina_Charo";
     public static final String ID = "vcg."+NAME.toLowerCase();
-    public static final String VERSION = "1.0.0-BETA";
+    public static final String VERSION = "1.0.0";
     public static final String DESCRIPTION = "图片小助手";
     public static final String COMMAND_PREFIX = "!!";
     public static final String IMG_STORAGE_PATH = "./data/image/"+NAME+"/";
