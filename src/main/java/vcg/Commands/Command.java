e14e3f12d8cbef8938f87a93170b1e3c395f7645
VatinaCharo <njuscimenphy@gmail.com>
2021-09-29 19:56:06 +0800

    refactor code and add help info framework

---
+++ src/main/java/vcg/Commands/Command.java
@@ -0,0 +1,28 @@
+package vcg.Commands;
+
+import net.mamoe.mirai.event.events.GroupMessageEvent;
+import net.mamoe.mirai.message.data.MessageChain;
+import vcg.Config.Config;
+
+public interface Command {
+    String getName();
+
+    /**
+     * 重载方法以添加额外的帮助信息
+     *
+     * 默认情况示例：
+     * ```text
+     * !!help
+     * >>>  Help Info Manuel
+     *      --------------------
+     *      !!ping
+     * ```
+     *
+     * @return help info
+     */
+    default String info() {
+        return Config.INSTANCE.getCommandPrefix()+getName();
+    }
+
+    void onCommand(GroupMessageEvent event);
+}
