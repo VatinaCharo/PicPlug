cb288be2e760c453b10fc4fb44f0133298ff3e0b
VatinaCharo <njuscimenphy@gmail.com>
2021-09-30 11:28:42 +0800

    update to v1.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,9 +8,10 @@ plugins {
 }
 
 group = "vcg"
-version = "1.0.0"
+version = "1.1.0"
 
 repositories {
     maven("https://maven.aliyun.com/repository/public")
+    mavenLocal()
     mavenCentral()
 }
--- src/main/java/vcg/Utils/Resources.java
+++ src/main/java/vcg/Utils/Resources.java
@@ -4,8 +4,9 @@ public class Resources {
     public static final String NAME_CN = "图片小助手";
     public static final String NAME = "PicPlug";
     public static final String AUTHOR = "Vatina_Charo";
+//    public static final Long QQ = 3067710367L;
     public static final String ID = "vcg."+NAME.toLowerCase();
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
     public static final String DESCRIPTION = "图片小助手";
     public static final String COMMAND_PREFIX = "!!";
     public static final String IMG_STORAGE_PATH = "./data/image/"+NAME+"/";
